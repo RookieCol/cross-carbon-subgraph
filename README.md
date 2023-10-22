80fe9f81350cae9d0c015bd21d4dcecaeeab573a
rookie <juan.diegoam@hotmail.com>
2023-10-22 07:52:33 -0500

    :art: Redefined entities needed

---
+++ README.md
@@ -0,0 +1,36 @@
+
+# Green Carbon subgraph
+
+Green Carbon Calculator: decentralized calculator
+
+### Cross Carbon - The Graph studio:
+
+Repository url: https://api.studio.thegraph.com/query/56236/crosscarbon/v0.0.3
+
+## Quick start 🏄
+
+Prerequisites: [Node (v19 LTS)](https://nodejs.org/en/download/) and [Git](https://git-scm.com/downloads)
+
+> Clone the repository:
+
+```
+git clone https://github.com/salviega/green-carbon-calculator-subgraph.git
+```
+
+> Install dependencies:
+
+```
+yarn
+```
+
+> Run the project:
+
+```
+yarn chain
+```
+
+## Authors 🏗
+
+[Rookiecol](https://github.com/RookieCol)
+
+[salviega](https://github.com/salviega)
--- src/carbon.ts
+++ src/carbon.ts
@@ -1,59 +1,40 @@
 import {
-  Approval as ApprovalEvent,
   BougthCarbonCredits as BougthCarbonCreditsEvent,
   BougthCarbonCreditsCrosschain as BougthCarbonCreditsCrosschainEvent,
   GroceryCarbonFootprintOffset as GroceryCarbonFootprintOffsetEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
   RetiredCarbonCredits as RetiredCarbonCreditsEvent,
   RetiredCarbonCreditsCrosschain as RetiredCarbonCreditsCrosschainEvent,
   Transfer as TransferEvent,
   TransferCrosschain as TransferCrosschainEvent,
   TransferFromCrosschain as TransferFromCrosschainEvent,
-  TravelCarbonFootprintOffset as TravelCarbonFootprintOffsetEvent
-} from "../generated/Carbon/Carbon"
+  TravelCarbonFootprintOffset as TravelCarbonFootprintOffsetEvent,
+} from "../generated/Carbon/Carbon";
 import {
-  Approval,
   BougthCarbonCredits,
   BougthCarbonCreditsCrosschain,
   GroceryCarbonFootprintOffset,
-  OwnershipTransferred,
   RetiredCarbonCredits,
   RetiredCarbonCreditsCrosschain,
   Transfer,
   TransferCrosschain,
   TransferFromCrosschain,
-  TravelCarbonFootprintOffset
-} from "../generated/schema"
-
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-  entity.value = event.params.value
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+  TravelCarbonFootprintOffset,
+} from "../generated/schema";
 
 export function handleBougthCarbonCredits(
   event: BougthCarbonCreditsEvent
 ): void {
   let entity = new BougthCarbonCredits(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.buyer = event.params.buyer
-  entity.amount = event.params.amount
+  );
+  entity.buyer = event.params.buyer;
+  entity.amount = event.params.amount;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleBougthCarbonCreditsCrosschain(
@@ -61,16 +42,16 @@ export function handleBougthCarbonCreditsCrosschain(
 ): void {
   let entity = new BougthCarbonCreditsCrosschain(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.buyer = event.params.buyer
-  entity.amount = event.params.amount
-  entity.network = event.params.network
+  );
+  entity.buyer = event.params.buyer;
+  entity.amount = event.params.amount;
+  entity.network = event.params.network;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleGroceryCarbonFootprintOffset(
@@ -78,38 +59,22 @@ export function handleGroceryCarbonFootprintOffset(
 ): void {
   let entity = new GroceryCarbonFootprintOffset(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.requestId = event.params.requestId
-  entity.moneySpentProteins = event.params.moneySpentProteins
-  entity.moneySpentFats = event.params.moneySpentFats
-  entity.moneySpentCarbs = event.params.moneySpentCarbs
-  entity.proteinsEmission = event.params.proteinsEmission
-  entity.fatsEmission = event.params.fatsEmission
-  entity.carbsEmission = event.params.carbsEmission
-  entity.foodEmission = event.params.foodEmission
-  entity.buyer = event.params.buyer
+  );
+  entity.requestId = event.params.requestId;
+  entity.moneySpentProteins = event.params.moneySpentProteins;
+  entity.moneySpentFats = event.params.moneySpentFats;
+  entity.moneySpentCarbs = event.params.moneySpentCarbs;
+  entity.proteinsEmission = event.params.proteinsEmission;
+  entity.fatsEmission = event.params.fatsEmission;
+  entity.carbsEmission = event.params.carbsEmission;
+  entity.foodEmission = event.params.foodEmission;
+  entity.buyer = event.params.buyer;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  entity.save();
 }
 
 export function handleRetiredCarbonCredits(
@@ -117,16 +82,16 @@ export function handleRetiredCarbonCredits(
 ): void {
   let entity = new RetiredCarbonCredits(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.buyer = event.params.buyer
-  entity.amount = event.params.amount
-  entity.certificateId = event.params.certificateId
+  );
+  entity.buyer = event.params.buyer;
+  entity.amount = event.params.amount;
+  entity.certificateId = event.params.certificateId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRetiredCarbonCreditsCrosschain(
@@ -134,47 +99,47 @@ export function handleRetiredCarbonCreditsCrosschain(
 ): void {
   let entity = new RetiredCarbonCreditsCrosschain(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.buyer = event.params.buyer
-  entity.amount = event.params.amount
-  entity.network = event.params.network
+  );
+  entity.buyer = event.params.buyer;
+  entity.amount = event.params.amount;
+  entity.network = event.params.network;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
   let entity = new Transfer(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.value = event.params.value
+  );
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.value = event.params.value;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTransferCrosschain(event: TransferCrosschainEvent): void {
   let entity = new TransferCrosschain(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.amount = event.params.amount
-  entity.network = event.params.network
+  );
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.amount = event.params.amount;
+  entity.network = event.params.network;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTransferFromCrosschain(
@@ -182,18 +147,18 @@ export function handleTransferFromCrosschain(
 ): void {
   let entity = new TransferFromCrosschain(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.amount = event.params.amount
-  entity.network = event.params.network
+  );
+  entity.sender = event.params.sender;
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.amount = event.params.amount;
+  entity.network = event.params.network;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTravelCarbonFootprintOffset(
@@ -201,18 +166,18 @@ export function handleTravelCarbonFootprintOffset(
 ): void {
   let entity = new TravelCarbonFootprintOffset(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.requestId = event.params.requestId
-  entity.distance = event.params.distance
-  entity.nights = event.params.nights
-  entity.flightEmission = event.params.flightEmission
-  entity.hotelEmission = event.params.hotelEmission
-  entity.travelEmission = event.params.travelEmission
-  entity.buyer = event.params.buyer
+  );
+  entity.requestId = event.params.requestId;
+  entity.distance = event.params.distance;
+  entity.nights = event.params.nights;
+  entity.flightEmission = event.params.flightEmission;
+  entity.hotelEmission = event.params.hotelEmission;
+  entity.travelEmission = event.params.travelEmission;
+  entity.buyer = event.params.buyer;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
